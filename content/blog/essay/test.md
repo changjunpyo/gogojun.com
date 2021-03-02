cb0d6db8ea6eb9755b72abb50a563067e8b17011
JP <changjunpyo@gmail.com>
2021-03-02 15:56:57 +0900

    gogojun.com base setting

---
+++ content/blog/essay/test.md
@@ -0,0 +1,8 @@
+---
+title: test
+date: 2021-03-02 15:03:75
+category: essay
+thumbnail: { thumbnailSrc }
+draft: false
+---
+
--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,27 +1,27 @@
 module.exports = {
-  title: `Bee starter`,
+  title: `GOGOJUN`,
   description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  author: `[gogojun]`,
+  introduction: ``,
+  siteUrl: `https://gogojun.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `changjunpyo`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
-    linkedin: ``, // Your LinkedIn account
+    linkedin: `changjunpyo`, // Your LinkedIn account
   },
-  icon: `content/assets/felog.png`, // Add your favicon
+  icon: `content/assets/golang-vector-icon.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'changjunpyo/gogojun.com', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'gogojun',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
